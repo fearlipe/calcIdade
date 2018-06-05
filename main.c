80663f82ade0d8d526fc50281370c74d63ae48c7
fearlipe <avelarxd@gmail.com>
2018-06-05 17:53:49 -0300

    Calcular idade C

---
+++ main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	int ano;
+	printf("Digite um ano: ");
+	scanf("%d", &ano);
+	printf("A idade da pessoa eh %d", 2018 - ano );
+	return 0;	
+}
